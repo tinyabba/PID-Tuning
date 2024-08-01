8480b9602e1254960f3b3bdeb0c034cf3d4cebbc
Valentina <valentina.abbattista@mail.polimi.it>
2024-08-01 15:35:24 +0200

    Fixed experiment 6

--- main6.py
+++ main6.py
@@ -15,10 +15,19 @@ from experimental.runner_opt import Runner_opt
 
 warnings.filterwarnings("ignore")
 
+"""
+PIDTUNING ALGORITHM, EXPERIMENT 6:
+    - rho_0 < 0.3
+    - 26 possible PID tuples
+    - noise_sigma = 0.001
+    - horizon = 100000
+    - rho_0_ub = 0.4
+"""
+
 experiment = 6
 
 #Open json file
-f = open('config/testcase_synt_1.json')
+f = open('config/testcase_synt_4.json')
 param_dict = json.load(f)
 
 horizon = param_dict['horizon']
@@ -40,8 +49,7 @@ y_0 = 1
 #Define dictionary for the errors of the algorithms
 optimal = "optimal"
 pidtuning = "pidtuning"
-ziegler_nichols = "ziegler_nichols"
-alg_list = [optimal, pidtuning, ziegler_nichols]
+alg_list = [optimal, pidtuning]
 errors = {alg: np.zeros((n_trials, horizon)) for alg in alg_list}
 
 #Define noises
@@ -52,6 +60,7 @@ out_noise = np.random.normal(0, sigma, (n_trials, horizon, m))
 
 
 #Define range of possible PID parameters
+print("Defining range of possible PID parameters")
 log_space = np.logspace(0, 1, num=100, base=10)
 
 K_P_range_start = 0.0
@@ -71,6 +80,7 @@ K_D_range = (log_space - log_space.min()) / (log_space.max() - log_space.min()) 
 
 
 #Build list of ammissible PID parameters
+print("Building list of admissible PID parameters")
 pid_actions = []
 for K in list(itertools.product(K_P_range, K_I_range, K_D_range)):
     bar_A = utils.compute_bar_a(A, b, c, K)
@@ -79,37 +89,7 @@ for K in list(itertools.product(K_P_range, K_I_range, K_D_range)):
 
 pid_actions = np.array(pid_actions)
 n_arms = pid_actions.shape[0]
-
-
-
-#Run optimal algorithm
-optimal_errors_experiment = f"optimal_errors{experiment}.npy"
-K_opt_experiment = f"K_opt_{experiment}.npy"
-K_opt_idx_experiment = f"K_opt_idx_{experiment}.npy"
-all_errors_experiment = f"all_errors_{experiment}.npy"
-
-env = PIDTuningEnvironment(A, b, c, n, p, m, y_0, horizon, noise, out_noise, n_trials)
-print('Running Optimal algorithm')
-
-all_errors = np.zeros((n_arms, n_trials, horizon))
-np.save(optimal_errors_experiment, all_errors)
-all_SSE = np.zeros((n_arms, n_trials))
-K_opt_idx = np.zeros(n_trials)
-K_opt = np.zeros((n_trials, 3, 1))
-for i, K in enumerate(pid_actions):
-    print("Running simulation ", i)
-    runner_opt = Runner_opt(env, n_trials, horizon, 3, n_arms, pid_actions)
-    all_errors[i] = runner_opt.perform_simulations(K, i, experiment)
-    for trial_i in range(n_trials):
-        all_SSE[i, trial_i] = np.sum(np.power(all_errors[i, trial_i],2))
-for trial_i in range(n_trials):
-    K_opt_idx[trial_i] = np.argmin(all_SSE[:, trial_i])
-    K_opt[trial_i] = pid_actions[int(K_opt_idx[trial_i])]
-    errors[optimal][trial_i,:] = all_errors[int(K_opt_idx[trial_i]), trial_i, :]
-np.save(optimal_errors_experiment, errors[optimal])
-np.save(K_opt_idx_experiment, K_opt_idx)
-np.save(K_opt_experiment, K_opt)
-np.save(all_errors_experiment, all_errors)
+print("The list of admissible PID parameters contains", n_arms, "elements")
 
 
 
@@ -164,5 +144,4 @@ print('Running PID Tuning')
 runner = Runner(env, agent, n_trials, horizon, 3, n_arms, pid_actions)
 errors[pidtuning] = runner.perform_simulations(experiment)
 np.save(pid_tuning_errors_experiment, errors[pidtuning])
-
 
